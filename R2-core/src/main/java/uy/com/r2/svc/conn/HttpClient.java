ea3fe79ff1f425a47659c57b06c86a763bc805c8
gcamargo01 <gcamargo1@gmail.com>
2017-07-23 13:51:21 -0300

    Ref. implementation

---
+++ R2-core/src/main/java/uy/com/r2/svc/conn/HttpClient.java
@@ -0,0 +1,183 @@
+/* HTTPClient.java */
+package uy.com.r2.svc.conn;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import org.apache.log4j.Logger;
+import uy.com.r2.core.api.SvcRequest;
+import uy.com.r2.core.api.SvcResponse;
+import uy.com.r2.core.api.AsyncService;
+import uy.com.r2.core.api.ConfigItemDescriptor;
+import uy.com.r2.core.api.Configuration;
+import uy.com.r2.core.api.SvcMessage;
+
+
+/** HTTP client connector.
+ * It invokes a HTTP service to getDataField the response.
+ * @author G.Camargo
+ */
+public class HttpClient implements AsyncService {
+    private static final Logger log = Logger.getLogger(HttpClient.class);
+    private Map<String,String> svcUrl = new HashMap<String,String>();
+    private int sleepTimeWait = 10;
+    
+    /** Get the configuration descriptors of this module.
+     * @return ConfigItemDescriptor List
+     */
+    @Override
+    public List<ConfigItemDescriptor> getConfigDescriptors() {
+        LinkedList<ConfigItemDescriptor> l = new LinkedList<ConfigItemDescriptor>();
+        l.add( new ConfigItemDescriptor( "Service.*", ConfigItemDescriptor.URL,
+                "Service names and URL to call", null));
+        l.add( new ConfigItemDescriptor( "SleepWaitingTime", ConfigItemDescriptor.INTEGER,
+                "Sleep time waiting for a response, in mS", "10"));
+        return l;
+    }
+    
+    private void setConfiguration( Configuration cfg) throws Exception {
+        svcUrl = cfg.getStringMap( "Service.*");
+        sleepTimeWait = cfg.getInt( "SleepTimeWait");
+    }
+
+    /** Service call.
+     * @param req Invocation message
+     * @return SvcResponse message
+     * @throws Exception Unexpected error, the responseCode will be lower than 0
+     */
+    @Override
+    public SvcMessage onRequest( SvcRequest req, Configuration cfg) throws Exception {
+        setConfiguration( cfg);
+        int to = req.getTimeOut();
+        long at = req.getAbsoluteTime();
+        if( to > 0) {
+            at += to;
+        } else {
+            at = Long.MAX_VALUE;
+        }
+        String r = invoke( req, at);
+        Map<String, List<Object>> d = SvcMessage.addToPayload( null, "Data", r);
+        SvcResponse resp = new SvcResponse( d, 0, req);
+        return resp;
+    }
+
+    /** Get the status report.
+     * @return Variable and value map
+     */
+    @Override
+    public Map<String, Object> getStatusVars() {
+        Map<String,Object> m = new HashMap();
+        m.put( "Version", "$Revision: 1.1 $");
+        return m;
+    }
+
+    /** Release all the allocated resources. */
+    @Override
+    public void shutdown() {
+    }
+
+    private String invoke( SvcRequest params, long endTime) 
+            throws Exception {
+        log.debug( "invoke " + params); 
+        String service = params.getServiceName();
+        String strUrl = svcUrl.get( service);
+        if( strUrl == null) {
+            if( service.contains( "/")) {
+                int i = service.lastIndexOf( '/');
+                StringBuilder su = new StringBuilder( service);
+                su.append( "?");
+                for( String k: params.getPayload().keySet()) {
+                    su.append( k);
+                    su.append( '=');
+                    su.append( params.get( k));
+                    su.append( '&');
+                }
+                service = service.substring( i + 1);
+                strUrl = su.toString();
+            } else {
+                log.warn( "Service w/o URL defined: " + service);
+                throw new Exception( "Service w/o URL defined: " + service); 
+            }
+        }
+        log.trace( "strUrl=" + strUrl + " svc=" + service); 
+        try {
+            HashMap<String,String> vv = new HashMap<String,String>();
+            int p = -1;
+            for( ; ; ) {
+                String q = strUrl.substring( p + 1);
+                //log.trace( "q=" + q);
+                int np = q.indexOf( "$(");
+                if( np < 0) {
+                    break;
+                }
+                p += np + 1; 
+                //log.trace( "p=" + p + " " + strUrl.substring( 0, p));
+                int ep = strUrl.substring( p).indexOf( ')');
+                if( ep > 0) {
+                    ep += p;
+                    //log.trace( "pp=" + ep + " " + strUrl.substring( 0, ep));
+                    String var = strUrl.substring( p + 2, ep);
+                    //log.trace( "var=" + var);
+                    String value = "" + params.get( var);
+                    if( value == null) {
+                        value = "";
+                    }
+                    vv.put( var, value);
+                    log.debug( "vv(" + var + ")=" + value); 
+                }
+            }
+            for( String k: vv.keySet()) {
+                strUrl = strUrl.replace( "$(" + k + ")", vv.get( k));
+            }
+            log.debug( "***** strUrl2=" + strUrl + " p=" + params); 
+            URL url = new URL( strUrl);
+            HttpURLConnection conn = ( HttpURLConnection)url.openConnection();
+            conn.setRequestMethod( "GET");
+            conn.setRequestProperty( "User-Agent", "R2");
+            //conn.setRequestProperty( "Accept", "application/json");
+            //log.trace( "conn.getResponseMessage=" + conn.getResponseMessage());
+            if( conn.getResponseCode() != 200) {
+                throw new RuntimeException( "Failed service '" + service + "' " 
+                        + strUrl + ", HTTP error code: " 
+                        + conn.getResponseCode());
+            }    
+            InputStream is = conn.getInputStream();
+            BufferedReader br = new BufferedReader( new InputStreamReader( is));
+            StringBuilder sb = new StringBuilder();
+            for( ; ;) {
+                if( is.available() == 0) {
+                    if( System.currentTimeMillis() > endTime) {
+                        break;
+                    }
+                    Thread.sleep( sleepTimeWait);
+                }
+                String line = br.readLine();
+                if( line == null) {
+                    break;
+                }    
+                sb.append( line);
+                sb.append( '\n');
+            }
+            conn.disconnect();
+            log.trace( "resp: " + sb.toString());
+            return sb.toString();
+        } catch( Exception x) {
+            log.warn( x + " invoking " + service + " " + strUrl, x);
+            throw new Exception( x + " invoking " + service + " " + strUrl, x);
+        }
+    }
+
+    @Override
+    public SvcResponse onResponse( SvcResponse resp, Configuration cfg) throws Exception {
+        throw new UnsupportedOperationException( "Not used."); 
+    }
+    
+}
+
+
---
+++ R2-core/src/main/java/uy/com/r2/svc/conn/JdbcService.java
@@ -0,0 +1,203 @@
+/* JdbcService.java */
+package uy.com.r2.svc.conn;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.LinkedHashMap;
+import org.apache.log4j.Logger;
+import uy.com.r2.core.api.SvcRequest;
+import uy.com.r2.core.api.SvcResponse;
+import uy.com.r2.core.api.AsyncService;
+import uy.com.r2.core.api.ConfigItemDescriptor;
+import uy.com.r2.core.api.Configuration;
+import uy.com.r2.core.api.SvcMessage;
+
+
+/** JDBC service module.
+ * @author G.Camargo
+ */
+public class JdbcService implements AsyncService {
+    private static final Logger log = Logger.getLogger(JdbcService.class);
+    private String driverClass = "";
+    private String url = "";
+    private String user = "";
+    private String password = "";
+    private Map<String,ServiceInfo> svcs = new HashMap();
+    private Connection conn = null;
+    private boolean firstTime = true;
+     
+    /** Get the configuration descriptors of this module.
+     * @return ConfigItemDescriptor List
+     */
+    @Override
+    public List<ConfigItemDescriptor> getConfigDescriptors() {
+        LinkedList<ConfigItemDescriptor> l = new LinkedList();
+        l.add( new ConfigItemDescriptor( "Driver", ConfigItemDescriptor.STRING,
+                "JDBC Driver class", null));
+        l.add( new ConfigItemDescriptor( "URL", ConfigItemDescriptor.STRING,
+                "JDBC URL", null));
+        l.add( new ConfigItemDescriptor( "User", ConfigItemDescriptor.STRING,
+                "JDBC User", null));
+        l.add( new ConfigItemDescriptor( "Password", ConfigItemDescriptor.STRING,
+                "JDBC Password", null));
+        l.add( new ConfigItemDescriptor( "Service.*.SQL", ConfigItemDescriptor.STRING,
+                "Service and SQL sentence", null));
+        l.add( new ConfigItemDescriptor( "Service.*.Params", ConfigItemDescriptor.STRING,
+                "Service and SQL parameters separated by coma (,)", null));
+        l.add( new ConfigItemDescriptor( "Service.*.RowName", ConfigItemDescriptor.STRING,
+                "Name of a tuple", "Row"));
+        return l;
+    }
+    
+    /** Inject configuration to this module.
+     * @param cfg Variable and value map
+     * @throws Exception Unexpected error
+     */
+    private void setConfiguration( Configuration cfg) throws Exception {
+        if( !cfg.isChanged()) {
+            return;
+        }
+        firstTime = false;
+        driverClass = cfg.getString( "Driver");
+        url = cfg.getString( "URL");
+        user = cfg.getString( "User");
+        password = cfg.getString( "Password");
+        if( cfg.getBoolean( "SeptUpTest")) {
+            getConnection();  // Try!
+        }  
+        Map<String,String> svcsSQL = cfg.getStringMap( "Service.*.SQL");
+        Map<String,String> svcsParams = cfg.getStringMap( "Service.*.Params");
+        Map<String,String> svcsRowName = cfg.getStringMap( "Service.*.RowName");
+        svcs = new HashMap();
+        for( String k: svcsSQL.keySet()) {
+            ServiceInfo si = new ServiceInfo();
+            si.sqlSentence = svcsSQL.get( k);
+            si.paramNames = svcsParams.get( k).split( ",");
+            si.rowName = svcsRowName.get( k);
+            if( si.rowName == null) {
+                si.rowName = "Row";
+            }
+            svcs.put( k, si);
+            log.debug( "Service " + k + " " + si.sqlSentence);
+        }
+        log.debug( "Services defined: " + svcs.size());
+    }
+
+    /** Service call.
+     * @param req Invocation message
+     * @param cfg Module configuration
+     * @return SvcResponse message
+     * @throws Exception Unexpected error, the responseCode will be lower than 0
+     */
+    @Override
+    public SvcResponse onRequest( SvcRequest req, Configuration cfg) throws Exception {
+        setConfiguration( cfg);
+        String svcName = req.getServiceName();
+        ServiceInfo si = svcs.get( svcName);
+        if( si == null) {
+            throw new Exception( SvcResponse.MSG_INVALID_SERVICE + req.getServiceName());
+        }
+        Map<String,List<Object>> r = execute( req.getPayload(), si);
+        SvcResponse resp = new SvcResponse( r, 0, req);
+        return resp;
+    }
+
+    /** Get the status report.
+     * @return Variable and value map
+     */
+    @Override
+    public Map<String, Object> getStatusVars() {
+        Map<String,Object> m = new HashMap();
+        m.put( "Version", "$Revision: 1.1 $");
+        for( String s: svcs.keySet()) {
+            if( svcs.get( s).uses > 0) {
+                m.put( s + ".Uses", "" + svcs.get( s).uses);
+                m.put( s + ".Errors", "" + svcs.get( s).errors);
+                m.put( s + ".Time", "" + ( svcs.get( s).time / svcs.get( s).uses));
+            }    
+        }
+        return m;
+    }
+
+    /** Release all the allocated resources. */
+    @Override
+    public void shutdown() {
+        try {
+            conn.close();
+        } catch( Exception x) { }
+        conn = null;
+    }
+
+    private Connection getConnection() throws Exception {
+        if( conn == null) {
+            Class.forName( driverClass);
+            conn = DriverManager.getConnection( url, user, password);
+        }
+        return conn;
+    }
+    
+    private Map<String,List<Object>> execute( Map<String,List<Object>> input, ServiceInfo si) 
+            throws Exception {
+        ++si.uses;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            long t0 = System.currentTimeMillis();
+            log.trace( "sqlSentence=" + si.sqlSentence);
+            ps = getConnection().prepareStatement( si.sqlSentence);
+            for( int i = 0; i < si.paramNames.length; ++i) {
+                Object o = null;
+                try {
+                    o = input.get( si.paramNames[ i]).get( 0);
+                } catch( Exception xx) { }
+                ps.setObject( i + 1, o);
+                log.trace( "arg" + (i + 1) + "=" + o);
+            }
+            if( ps.execute()) {
+                rs = ps.getResultSet();
+            }
+            ResultSetMetaData rsmd = rs.getMetaData();
+            Map<String,List<Object>> resp = new LinkedHashMap();
+            while( rs != null && rs.next()) {
+                Map<String,List<Object>> row = new LinkedHashMap();
+                for( int i = 0; i < rsmd.getColumnCount(); ++i) {
+                    SvcMessage.addToPayload( row, rsmd.getColumnName( i + 1), rs.getString( i + 1));
+                }
+                SvcMessage.addToPayload( resp, si.rowName, row);
+            }    
+            si.time += ( int)(System.currentTimeMillis() - t0);
+            return resp;
+        } catch( Exception x) {
+            ++si.errors;
+            log.warn( "Unexpcected Exception on JDBC " + url, x);
+            throw new Exception( "Unexpcected Exception on JDBC " + url, x);
+        } finally {   // Allways release 
+            try { rs.close(); } catch( Exception xx) { }
+            try { ps.close(); } catch( Exception xx) { }
+        }
+    }
+
+    @Override
+    public SvcResponse onResponse( SvcResponse resp, Configuration cfg) throws Exception {
+        throw new UnsupportedOperationException( "Not supported."); 
+    }
+    
+    private class ServiceInfo {
+        String sqlSentence;
+        String paramNames[];
+        String rowName;
+        int uses = 0;
+        int errors = 0;
+        int time = 0;
+    }
+    
+}
+
+
