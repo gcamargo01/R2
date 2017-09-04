51e1e6ed9de15dc32981be73310125191d15f35a
gcamargo01 <gcamargo1@gmail.com>
2017-09-03 23:22:19 -0300

    include req

--- R2-api/src/main/java/uy/com/r2/core/api/SvcException.java
+++ R2-api/src/main/java/uy/com/r2/core/api/SvcException.java
@@ -2,31 +2,36 @@
 package uy.com.r2.core.api;
 
 /** R2 service exception.
- * An Exception with message and errorCode. 
+ * An Exception with a message (error or failed action) a errorCode and a SvcRequest. 
  * @author G.Camargo
  */
 public class SvcException extends Exception {
     private static final long serialVersionUID = 1L;
 
     private final int errorCode;
+    private final SvcRequest req;
     
     /** Pack information about the error.
      * @param reasonOrAction Reason of the failure or current action
      * @param errorCode Numeric code, may be lower than 0 for fatal errors
+     * @param req Request message
      */
-    public SvcException( String reasonOrAction, int errorCode) {
+    public SvcException( String reasonOrAction, int errorCode, SvcRequest req) {
         super( reasonOrAction);
         this.errorCode = errorCode;
+        this.req = req;
     }
 
     /** Pack information about the exception.
      * @param reasonOrAction Reason of the failure or current action
      * @param errorCode Numeric code, may be lower than 0 for fatal errors
      * @param th Previous Exception 
+     * @param req Request message
      */
-    public SvcException( String reasonOrAction, int errorCode, Throwable th) {
+    public SvcException( String reasonOrAction, int errorCode, Throwable th, SvcRequest req) {
         super( reasonOrAction, th);
         this.errorCode = errorCode;
+        this.req = req;
     }
 
     /** Get the error code.
@@ -36,5 +41,23 @@ public class SvcException extends Exception {
         return errorCode;        
     }
 
+    /** Get the original request that origin the failure.
+     * @return SvcRequest
+     */
+    public SvcRequest getRequest( ) {
+        return req;        
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append( super.toString());
+        s.append( " ErrorCode: ");
+        s.append( errorCode);
+        s.append( " Req: ");
+        s.append( req.toString());
+        return s.toString();
+    }
+    
 }
 
