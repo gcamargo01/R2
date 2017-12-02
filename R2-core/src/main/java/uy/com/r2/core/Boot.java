69ac18e3b00f63cc5babac68ea20a04c464dbfc5
gcamargo01 <gcamargo1@gmail.com>
2017-12-01 23:37:23 -0300

    simple default cfg

--- R2-core/src/main/java/uy/com/r2/core/Boot.java
+++ R2-core/src/main/java/uy/com/r2/core/Boot.java
@@ -2,30 +2,23 @@
 package uy.com.r2.core;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import org.apache.log4j.Logger;
 import uy.com.r2.core.api.SvcRequest;
-import uy.com.r2.core.api.ConfigItemDescriptor;
 import uy.com.r2.core.api.Configuration;
-import uy.com.r2.core.api.Module;
 import uy.com.r2.svc.conn.HttpClient;
-import uy.com.r2.svc.conn.JdbcService;
-import uy.com.r2.svc.conn.MicroHttpServer;
-import uy.com.r2.svc.tools.FilePathSynchronizer;
 import uy.com.r2.svc.tools.FileServices;
 import uy.com.r2.svc.tools.Json;
-import uy.com.r2.svc.tools.SvcAvailServers;
-import uy.com.r2.svc.tools.ToHtml;
 
 /** Startup implementation.
  * Its: functions include: <br>
@@ -77,19 +70,33 @@ public class Boot {
         try {
             LOG.trace( "start " + localHost + " " + localPort + " " + rmtUrl);
             // Read previous configuration o basic config
-            Properties pr = readConfig( localHost, localPort, rmtUrl);
-            // Deploy initial pipe
-            for( int i = 0; pr.getProperty( "Module." + i) != null; ++i) {
-                String mod = pr.getProperty( "Module." + i);
-                String ki = "" + i + ".";
-                Configuration c = new Configuration();
-                for( String k: pr.stringPropertyNames()) {
-                    if( k.startsWith( ki)) {
-                        c.put( k.substring( ki.length()), pr.get( k));
-                    }    
+            Map<String,String> pr = readConfig( localHost, localPort, rmtUrl);
+            // Search modules key cm: Key -> ModuleName
+            TreeMap<String,String> km = new TreeMap();
+            for( String p: pr.keySet()) {
+                if( !p.contains( ".")) {
+                    km.put( p, pr.get( p));
                 }
-                LOG.trace( "Deploy " + mod + " " + c);
-                SvcCatalog.getCatalog().installModule( mod, c);
+            }
+            // Instance configurations: ModuleName -> Configuration
+            TreeMap<String,Configuration> cm = new TreeMap();
+            for( String k: km.keySet()) {
+                cm.put( km.get( k), new Configuration());
+            }
+            // Search for config items k + "."
+            for( String p: pr.keySet()) {
+                if( p.contains( ".")) {
+                    String k = p.substring( 0, p.indexOf( '.'));
+                    if( km.keySet().contains( k)) {  
+                        cm.get( km.get( k)).put( p.substring( k.length() + 1), pr.get( p));
+                    }
+                }
+            }
+            // Deploy modules , ordered by k
+            for( String k: km.keySet()) {
+                //c.put( "Monitor", true);
+                LOG.trace( "Deploy " + km.get( k) + " " + cm.get( km.get( k)));
+                SvcCatalog.getCatalog().installModule( km.get( k), cm.get( km.get( k)));
             }
         } catch ( Exception ex ) {
             LOG.warn( "" + ex, ex);
@@ -120,13 +127,13 @@ public class Boot {
         fos.close();
     }
     
-    private static Properties readConfig( String hostName, int localPort, String rmtUrl) throws Exception {
+    private static Map<String,String> readConfig( String hostName, int localPort, String rmtUrl) throws Exception {
         String r2Path = System.getProperty( "R2_PATH", "");
         if( r2Path.length() > 0 && !r2Path.endsWith( File.separator)) {
             r2Path += File.separator;
         }
         Properties pr = new Properties();
-        /* !!!! load its last confuration
+        // load its last confuration
         try {
             FileInputStream fi = new FileInputStream( r2Path + "R2.properties");
             pr.load( fi);
@@ -134,7 +141,6 @@ public class Boot {
         } catch( Exception x) {
             LOG.info( "Can't load R2.properties: " + x);
         }    
-        */
         if( pr.isEmpty()) {
             // calculate default server name
             String localUrl = "http://" + hostName + ":" + localPort;
@@ -142,16 +148,20 @@ public class Boot {
             // Set default cliendNode
             new SvcRequest( localName, 0, 0, null, null, 0).getClientNode();
             // configure
-            pr.putAll( DEFAULT_PIPE);
-            pr.put( "1.Port", "" + localPort);
-            pr.put( "2.LocalUrl", localUrl);
+            pr.putAll(DEFAULT_CFG);
+            pr.put( "01.Port", "" + localPort);
+            pr.put( "02.LocalUrl", localUrl);
             if( rmtUrl != null && !rmtUrl.isEmpty()) {
-                pr.put( "2.RemoteUrl", rmtUrl);
-                pr.put( "7.Url", rmtUrl);
+                pr.put( "02.RemoteUrl", rmtUrl);
+                pr.put( "07.Url", rmtUrl);
             }
         }
         LOG.trace( "Startup cfg " + pr);
-        return pr;
+        Map<String,String>m = new TreeMap();
+        for( Object p: pr.keySet()) {
+            m.put( "" + p, "" + pr.get( p));
+        }
+        return m;
     }
 
     public static String getHostName() {
@@ -180,57 +190,70 @@ public class Boot {
     }
     
 
-    static final Map<String,String> DEFAULT_PIPE = new HashMap();
+    static final Map<String,String> DEFAULT_CFG = new HashMap();
     static {
-        DEFAULT_PIPE.put( "Module.0", SvcCatalog.DISPATCHER_NAME);
-        DEFAULT_PIPE.put( "0.class", SimpleDispatcher.class.getName());
-        DEFAULT_PIPE.put( "0.DefaultServicePipeline", "SrvHtml,SrvJson,JdbcService,FileServices,SvcDeployer,SvcAvailServers");
-        DEFAULT_PIPE.put( "0.Pipeline._Undefined_", "ClntJson,HttpClient");
-        DEFAULT_PIPE.put( "0.Pipeline.Udp", "ClntJson,UdpClient");
-        DEFAULT_PIPE.put( "Module.1", MicroHttpServer.class.getSimpleName());
-        DEFAULT_PIPE.put( "1.class", MicroHttpServer.class.getName());
-        DEFAULT_PIPE.put( "1.Port", "8015");
-        DEFAULT_PIPE.put( "Module.2", SvcAvailServers.class.getSimpleName());
-        DEFAULT_PIPE.put( "2.class", SvcAvailServers.class.getName());
-        // Default add 2.LocalName
-        // Default add 2.LocalUrl
-        // Default add 2.RemotelUrl
-        DEFAULT_PIPE.put( "Module.3", "SrvHtml");
-        DEFAULT_PIPE.put( "3.class", ToHtml.class.getName());
-        DEFAULT_PIPE.put( "Module.4", "SrvJson");
-        DEFAULT_PIPE.put( "4.class", Json.class.getName());
-        DEFAULT_PIPE.put( "4.ToSerial", "false");
-        DEFAULT_PIPE.put( "4.ProcessRequest", "true");
-        DEFAULT_PIPE.put( "4.ProcessResponse", "true");
-        DEFAULT_PIPE.put( "Module.5", JdbcService.class.getSimpleName());
-        DEFAULT_PIPE.put( "5.class", JdbcService.class.getName());
-        DEFAULT_PIPE.put( "5.Driver", "org.apache.derby.jdbc.ClientDriver");
-        DEFAULT_PIPE.put( "5.URL", "jdbc:derby://localhost:1527/Test");
-        DEFAULT_PIPE.put( "5.User", "root");
-        DEFAULT_PIPE.put( "5.Password", "XXXX");
-        DEFAULT_PIPE.put( "5.Service.ListClients.SQL", "SELECT * FROM clients");
-        DEFAULT_PIPE.put( "5.Service.AddClient.SQL", "INSERT INTO clients(id,name) VALUES (?,?)");
-        DEFAULT_PIPE.put( "5.Service.AddClient.Params", "Id,Name");
-        DEFAULT_PIPE.put( "Module.6", "ClntJson");
-        DEFAULT_PIPE.put( "6.class", Json.class.getName());
-        DEFAULT_PIPE.put( "6.ToSerial", "true");
-        DEFAULT_PIPE.put( "6.ProcessRequest", "true");
-        DEFAULT_PIPE.put( "6.ProcessResponse", "true");
-        DEFAULT_PIPE.put( "Module.7", HttpClient.class.getSimpleName());
-        DEFAULT_PIPE.put( "7.class", HttpClient.class.getName());
-        DEFAULT_PIPE.put( "Module.8", FileServices.class.getSimpleName());
-        DEFAULT_PIPE.put( "8.class", FileServices.class.getName());
-        DEFAULT_PIPE.put( "Module.9", SvcDeployer.class.getSimpleName());
-        DEFAULT_PIPE.put( "9.class", SvcDeployer.class.getName());
-        
-        DEFAULT_PIPE.put( "Module.10", "UdpServer");
-        DEFAULT_PIPE.put( "10.class", uy.com.r2.svc.conn.UdpServer.class.getName());
-        DEFAULT_PIPE.put( "Module.11", "UdpClient");
-        DEFAULT_PIPE.put( "11.class", uy.com.r2.svc.conn.UdpClient.class.getName());
-        //DEFAULT_PIPE.put( "Module.10", FilePathSynchronizer.class.getSimpleName());
-        //DEFAULT_PIPE.put( "10.class", FilePathSynchronizer.class.getName());
-        //DEFAULT_PIPE.put( "10.Path.lib", "lib");
-        //DEFAULT_PIPE.put( "10.RemoteServer", "_Undefined_");
+        DEFAULT_CFG.put( "00", SvcCatalog.DISPATCHER_NAME);
+        DEFAULT_CFG.put( "00.class", SimpleDispatcher.class.getName());
+        DEFAULT_CFG.put( "00.DefaultServicePipeline", "SrvHtml,SrvJson,JdbcService,FileServices,SvcDeployer,SvcAvailServers");
+        DEFAULT_CFG.put( "00.Pipeline._Undefined_", "ClntJson,HttpClient");
+        DEFAULT_CFG.put( "00.Pipeline.Udp", "ClntJson,UdpClient");
+        DEFAULT_CFG.put( "00.Pipeline.Test", "JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,JavaScript,Echo");
+        DEFAULT_CFG.put( "01", uy.com.r2.svc.conn.MicroHttpServer.class.getSimpleName());
+        DEFAULT_CFG.put( "01.class", uy.com.r2.svc.conn.MicroHttpServer.class.getName());
+        DEFAULT_CFG.put( "01.Port", "8015");
+        DEFAULT_CFG.put( "02", uy.com.r2.svc.tools.SvcAvailServers.class.getSimpleName());
+        DEFAULT_CFG.put( "02.class", uy.com.r2.svc.tools.SvcAvailServers.class.getName());
+        // Default add 02.LocalName
+        // Default add 02.LocalUrl
+        // Default add 02.RemotelUrl
+        DEFAULT_CFG.put( "03", "SrvHtml");
+        DEFAULT_CFG.put( "03.class", uy.com.r2.svc.tools.ToHtml.class.getName());
+        DEFAULT_CFG.put( "04", "SrvJson");
+        DEFAULT_CFG.put( "04.class", uy.com.r2.svc.tools.Json.class.getName());
+        DEFAULT_CFG.put( "04.ToSerial", "false");
+        DEFAULT_CFG.put( "04.ProcessRequest", "true");
+        DEFAULT_CFG.put( "04.ProcessResponse", "true");
+        DEFAULT_CFG.put( "05", uy.com.r2.svc.conn.JdbcService.class.getSimpleName());
+        DEFAULT_CFG.put( "05.class", uy.com.r2.svc.conn.JdbcService.class.getName());
+        DEFAULT_CFG.put( "05.Driver", "org.apache.derby.jdbc.ClientDriver");
+        DEFAULT_CFG.put( "05.URL", "jdbc:derby://localhost:1527/Test");
+        DEFAULT_CFG.put( "05.User", "root");
+        DEFAULT_CFG.put( "05.Password", "XXXX");
+        DEFAULT_CFG.put( "05.Service.ListClients.SQL", "SELECT * FROM clients");
+        DEFAULT_CFG.put( "05.Service.AddClient.SQL", "INSERT INTO clients(id,name) VALUES (?,?)");
+        DEFAULT_CFG.put( "05.Service.AddClient.Params", "Id,Name");
+        DEFAULT_CFG.put( "06", "ClntJson");
+        DEFAULT_CFG.put( "06.class", Json.class.getName());
+        DEFAULT_CFG.put( "06.ToSerial", "true");
+        DEFAULT_CFG.put( "06.ProcessRequest", "true");
+        DEFAULT_CFG.put( "06.ProcessResponse", "true");
+        DEFAULT_CFG.put( "06.Monitor", "true");
+        DEFAULT_CFG.put( "07", HttpClient.class.getSimpleName());
+        DEFAULT_CFG.put( "07.class", HttpClient.class.getName());
+        // Default add 07.Url = rmtUrl
+        DEFAULT_CFG.put( "08", FileServices.class.getSimpleName());
+        DEFAULT_CFG.put( "08.class", FileServices.class.getName());
+        DEFAULT_CFG.put( "09", SvcDeployer.class.getSimpleName());
+        DEFAULT_CFG.put( "09.class", SvcDeployer.class.getName());       
+        DEFAULT_CFG.put( "10", "UdpServer");
+        DEFAULT_CFG.put( "10.class", uy.com.r2.svc.conn.UdpServer.class.getName());
+        DEFAULT_CFG.put( "11", "UdpClient");
+        DEFAULT_CFG.put( "11.class", uy.com.r2.svc.conn.UdpClient.class.getName());
+        DEFAULT_CFG.put( "12", "CfgRelay");
+        DEFAULT_CFG.put( "12.class", uy.com.r2.svc.tools.CfgRelay.class.getName());
+        //DEFAULT_CFG.put( "13", uy.com.r2.svc.tools.FilePathSynchronizer.class.getSimpleName());
+        //DEFAULT_CFG.put( "13.class", uy.com.r2.svc.tools.FilePathSynchronizer.class.getName());
+        //DEFAULT_CFG.put( "13.Path.lib", "lib");
+        //DEFAULT_CFG.put( "13.RemoteServer", "_Undefined_");
+        DEFAULT_CFG.put( "97", uy.com.r2.svc.tools.JavaScript.class.getSimpleName());
+        DEFAULT_CFG.put( "97.class", uy.com.r2.svc.tools.JavaScript.class.getName());
+        DEFAULT_CFG.put( "97.Script", "function requestService( rq) { rq.put( \"Field2\", \"ValueTwo\" + rq.get( \"Field\")); }" + 
+                "function responseService( rp) { }");
+        DEFAULT_CFG.put( "98", uy.com.r2.svc.tools.Echo.class.getSimpleName());
+        DEFAULT_CFG.put( "98.class", uy.com.r2.svc.tools.Echo.class.getName());
+        DEFAULT_CFG.put( "99", uy.com.r2.svc.tools.SvcTestingCaller.class.getSimpleName());
+        DEFAULT_CFG.put( "99.class", uy.com.r2.svc.tools.SvcTestingCaller.class.getName());
+        DEFAULT_CFG.put( "99.Pipeline", "Test");
     }
 
 }
