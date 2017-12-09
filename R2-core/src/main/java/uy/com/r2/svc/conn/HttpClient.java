95ff64a3d8aa5d496f5ac7e235941b0732c552af
gcamargo01 <gcamargo1@gmail.com>
2017-12-09 06:11:24 -0300

    exception message

--- R2-core/src/main/java/uy/com/r2/svc/conn/HttpClient.java
+++ R2-core/src/main/java/uy/com/r2/svc/conn/HttpClient.java
@@ -160,9 +160,9 @@ public class HttpClient implements AsyncService {
             conn.setRequestProperty( "Accept", "application/json");
             //log.trace( "conn.getResponseMessage=" + conn.getResponseMessage());
             if( conn.getResponseCode() != 200) {
-                throw new RuntimeException( "Failed call remote service '" + service + "' " 
-                        + strUrl + ", HTTP error code: " 
-                        + conn.getResponseCode());
+                throw new RuntimeException( "HTTP error code: " + conn.getResponseCode() + 
+                        "on call remote service '" + service + "' to " + strUrl
+                        );
             }    
             InputStream is = conn.getInputStream();
             BufferedReader br = new BufferedReader( new InputStreamReader( is));
