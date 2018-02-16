b1c628f8de217463af0ea8a5f4567dca7a99a750
gcamargo01 <gcamargo1@gmail.com>
2018-02-16 01:42:39 -0300

    comments

--- R2-api/src/main/java/uy/com/r2/core/api/Module.java
+++ R2-api/src/main/java/uy/com/r2/core/api/Module.java
@@ -15,13 +15,17 @@ public interface Module {
     /** Get the configuration descriptors of this module.
      * Each module must implement this method to give complete information about 
      * its configurable items.
+     * It is not mandatory to return the complete list of parameters used, but 
+     * it is considered a bad practice not to document them, and in each case it is warned.
      * @return ConfigItemDescriptor List
      */
     public List<ConfigItemDescriptor> getConfigDescriptors();
 
     /** Get the status report of the module.
-     * It may occurs at any time to get the current status of the module.
-     * The variables may include: Version, ServiceLevel, LastErrors, ...
+     * Used to get (and monitoring) current status of the module.
+     * The variables may include: Version, ServiceLevel, LastErrors, an so on.
+     * The core add: (usage) Count, ErrorCount, ServiceLevel(1=Ok, 0=Total failure), 
+     * and others when the service is monitored<br>
      * @return Variable and value map
      */
     public Map<String,Object> getStatusVars();
